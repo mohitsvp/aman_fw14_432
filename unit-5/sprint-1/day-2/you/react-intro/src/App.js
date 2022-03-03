5241e17d010aebabf041efe69629a45268f60aad
aman-638 <ar7009297@gmail.com>
2022-03-03 16:17:09 +0530

    we session

--- unit-5/sprint-1/day-2/you/react-intro/src/App.js
+++ unit-5/sprint-1/day-2/you/react-intro/src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+   const data=[
+     {name:"Aman",age:20},
+     {name:"ripu",age:22},
+     {name:"Tarniza",age:26},
+     {name:"kiran",age:28}
+   ]
   return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
+      <div>
+        {data.map((e) => (
+          <UserComponent name={e.name} age={e.age} />
+          //UserComponent(e)
+        ))}
+        </div>
   );
+}
+
+function UserComponent({name,age}){
+  return(
+    <div className='borderBox'>
+      <h2>hello {name}</h2>
+      <h3>Your age is {age}</h3>
+    </div>
+  )
 }
 
 export default App;
