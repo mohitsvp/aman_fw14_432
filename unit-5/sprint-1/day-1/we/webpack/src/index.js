8823998918ef9cfa671107fb12ef8a4d3795c2ff
aman-638 <ar7009297@gmail.com>
2022-03-03 08:52:47 +0530

    assignment

--- unit-5/sprint-1/day-1/we/webpack/src/index.js
+++ unit-5/sprint-1/day-1/we/webpack/src/index.js
@@ -1,6 +1,7 @@
 import {mult,add} from "./calc.js";
 import "./index.css";
 
+
 console.log(mult(4,4));
 
 
@@ -8,7 +9,40 @@ console.log(mult(4,4));
 console.log(add(2,2));
 
 const h1 = document.createElement("h1");
-h1.innerText="welcome to webpack";
+h1.innerText="welcome to webpack todo";
 h1.classList.add("redtext");
 
-document.querySelector("#root").appendChild(h1);
+document.querySelector("#root").append(h1);
+
+document.querySelector("#save_btn").addEventListener("click",save);
+
+
+function save(){
+    let data=document.querySelector("#note").value;
+    
+    document.querySelector(".th_css").style.border="1px solid red";
+    document.querySelector(".th_css").style.padding="10px";
+
+    let row=document.createElement('tr');
+    let td1=document.createElement('td');
+    td1.textContent=data;
+    td1.style.border="1px solid green";
+    td1.style.padding="10px";
+
+    let td2=document.createElement('button');
+    td2.textContent="Completed";
+    td2.style.marginLeft="10px";
+    td2.style.border="1px solid green";
+    td2.style.padding="10px";
+
+    td2.addEventListener("click",deleteNote);
+
+    row.append(td1,td2);
+
+   document.querySelector("#note_ui").append(row);
+   document.querySelector("#note_ui").style.marginTop="20px";
+}
+
+function deleteNote(event){
+  event.target.parentNode.remove();
+}
--- unit-5/sprint-1/day-1/we/webpack/webpack.config.js
+++ unit-5/sprint-1/day-1/we/webpack/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
+const HtmlWebpackPlugin=require('html-webpack-plugin');
+const {CleanWebpackPlugin}=require('clean-webpack-plugin');
 
 module.exports={
+    mode: 'development',
     entry:"./src/index.js",
     output:{
         path:path.resolve(__dirname,"build"),
@@ -8,7 +11,31 @@ module.exports={
     },
     module: {
         rules: [
-          { test: /\.css$/, use: ["style-loader",'css-loader'] },
+          { test: /\.css$/, 
+            use: ["style-loader",'css-loader'] 
+           },
+           {
+             test:/\.html$/,
+             use:['html-loader']
+           },
+              {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options:{
+                      name:'[name].[ext]',
+                      outputPath:"img"
+                    }
+                  },
+                ],
+              },
         ],
       },
+      plugins:[
+        new HtmlWebpackPlugin({
+          template:'index.html'
+        }),
+        new CleanWebpackPlugin(),
+      ]
 }
