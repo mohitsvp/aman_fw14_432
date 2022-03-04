5ede848b61e88527dc881156dac25257df5a8a66
aman-638 <ar7009297@gmail.com>
2022-03-04 16:18:33 +0530

    assignment

--- unit-5/sprint-1/day-3/you/react-state-props/src/components/counter.jsx
+++ unit-5/sprint-1/day-3/you/react-state-props/src/components/counter.jsx
@@ -4,9 +4,6 @@ export function Counter(){
     const [counter,setCounter] = useState(0);
     
     const handleAdd = (value) => {
-        if(counter + value <0){
-            return;
-        }
         setCounter(counter+value);
     };
 
@@ -14,14 +11,14 @@ export function Counter(){
     //     setCounter(counter-1); 
     // }
 
-    const handleReset = () => {
-        setCounter(0);
+    const handleDouble = () => {
+        setCounter(counter*2);
     }
     
     return <>
     <h3>Counter: {counter}</h3>
-    <button onClick={() => {handleAdd(1)}}>Add 1</button>
-    <button onClick={() => {handleAdd(-1)}}>Subtract 1</button>
-    <button onClick={handleReset}>Reset</button>
+    <button onClick={() => {handleAdd(1)}}>increment </button>
+    <button onClick={() => {handleAdd(-1)}}>decrement</button>
+    <button onClick={handleDouble}>double</button>
     </>
 }
