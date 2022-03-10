1df113f2c755bd441d6b7ec5a24dc670cbd64965
aman-638 <ar7009297@gmail.com>
2022-03-10 20:28:50 +0530

    assignment done

--- unit-5/sprint-2/day-7/form_ref/src/components/Forms.jsx
+++ unit-5/sprint-2/day-7/form_ref/src/components/Forms.jsx
@@ -1,41 +1,86 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import '../App.css';
 
 const Forms = () => {
-
+    const [list,setList] = useState([]);
     const [formData,setFormData] = useState({
-        username:"",
+        name:"",
         age:"",
-        email:"",
+        address:"",
+        department:"",
+        salary:"",
     });
-
     const handleChange = (e) => {
-       const {id,value} = e.target;
-    
-       setFormData({
-           ...formData,[id]:value
-       })
+        let {id,value,checked,type} = e.target;
+        value = type ==="checkbox" ? checked :value;
+        setFormData({
+            ...formData,[id]:value
+        })
+    }
+    const getData = () => {
+        axios.get(`http://localhost:3005/users`).then((res) => {
+            setList(res.data);
+        })
     }
     const submitForm = (e) => {
         e.preventDefault();
         axios.post("http://localhost:3005/users",formData).then(() => {
             alert("user created successfully");
-            // setFormData({
-            //     username:"",
-            //     age:"",
-            //     email:"",   
-            // })
+        }).then(() =>{
+             getData();
         })
     }
+  const {isMarried}= formData
   return (
     <div>
        <form onSubmit={submitForm}>
-           <h3>Sign up</h3>
-         <input id="username" type="text" onChange={handleChange} placeholder='enter username'/>
-         <input id="age" type="number" onChange={handleChange} placeholder='enter age' /> 
-         <input id="email" type="email" onChange={handleChange} placeholder='enter email' /> 
-         <input type="submit" value="sign up" />    
-       </form> 
+           <h3>Employee Details</h3>
+         <input id="name" type="text" onChange={handleChange} placeholder='enter name'/><br></br><br></br>
+         <input id="age" type="number" onChange={handleChange} placeholder='enter age' /> <br></br><br></br>
+         <input id="address" type="text" placeholder='address' onChange={handleChange} /><br></br><br></br>
+         Department
+         <select name="" id="department" onChange={handleChange}><br></br><br></br>
+          <option value="">----</option>
+          <option value="production">production</option>
+          <option value="quality">quality</option>
+          <option value="developing">developing</option>
+         </select>   
+          <br></br><br></br><br></br><br></br>
+         <input id="salary" type="number" placeholder='salary' onChange={handleChange} /><br></br><br></br>
+         martial State<input id="maritalState" type="checkbox"  onChange={handleChange} checked={isMarried} /><br></br><br></br>
+         <input type="submit" value="submit" />    
+       </form>
+
+       <div className='detail'>
+       <table className='table table-bordered text-center'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Address</th>
+                            <th>Department</th>
+                            <th>Salary</th>
+                            <th>Martial State</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+         {list.map((data,index) => (
+             
+                    <>   
+                        <tr>
+                            <td>{data.name}</td>
+                            <td>{data.age}</td>
+                            <td>{data.address}</td>
+                            <td>{data.department}</td>
+                            <td>{data.salary}</td>
+                            <td>{(data.maritalState===true) ? "true" :"false"}</td> 
+                        </tr>
+                  </>  
+            ))}
+        </tbody>
+        </table>
+       </div> 
     </div>
   )
 }
